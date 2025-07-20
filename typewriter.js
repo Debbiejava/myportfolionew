6b63c3a4722ee1861e028738cfb0f4b647b63bfb
Oluwaseun Deborah Adebayo <oluwaseuna294@gmail.com>
2025-07-20 16:03:47 +0100

    Styled events slider + roadmap cards + brighter footer

--- typewriter.js
+++ typewriter.js
@@ -13,3 +13,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
   type();
 });
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  loop: true
+});

e2c473bdbb785dc45d996e4989bf87d2063b8e23
Oluwaseun Deborah Adebayo <oluwaseuna294@gmail.com>
2025-07-20 22:09:18 +0100

    all edit

--- typewriter.js
+++ typewriter.js
@@ -11,5 +11,19 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  type();
+    type();
+    const swiper = new Swiper(".swiper", {
+        slidesPerView: 1,
+        spaceBetween: 20,
+        loop: true,
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+    });
+
 });
